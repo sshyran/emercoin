d0c70fbcbc921fd136b7102f918e7ac6e8738e41
EvgenijM86 <EvgenijM86@yandex.com>
2020-09-08 11:16:05 +0300

    rpc: fix getblockstats

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1866,8 +1866,7 @@ static UniValue getblockstats(const JSONRPCRequest& request)
     ret_all.pushKV("minfeerate", (minfeerate == MAX_MONEY) ? 0 : minfeerate);
     ret_all.pushKV("mintxsize", mintxsize == MAX_BLOCK_SERIALIZED_SIZE ? 0 : mintxsize);
     ret_all.pushKV("outs", outputs);
-    //emcTODO - replace this with our own block reward function
-    //ret_all.pushKV("subsidy", GetBlockSubsidy(pindex->nHeight, Params().GetConsensus()));
+    ret_all.pushKV("subsidy", pindex->nMint);
     ret_all.pushKV("swtotal_size", swtotal_size);
     ret_all.pushKV("swtotal_weight", swtotal_weight);
     ret_all.pushKV("swtxs", swtxs);
